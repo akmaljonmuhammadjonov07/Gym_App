768bd45ae762ee9c844148bd80503125db8af2d2
akmaljon <akmal@gmail.com>
2024-11-18 19:10:50 +0500

    Dashboard page qo'shildi

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/shared/navbar';
 import Auth from './pages/auth';
+import Dashboard from './pages/dashboard';
 import Home from './pages/home';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/auth' element={<Auth />} />
+				<Route path='/dashboard' element={<Dashboard />} />
 			</Routes>
 		</>
 	);
